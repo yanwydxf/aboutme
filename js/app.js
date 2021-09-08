7e89d7a40b448fad6de2e1e3b63ca0e678fae919
离岛 <yanwydxf@163.com>
2021-09-08 09:14:57 +0800

    init

---
+++ js/app.js
@@ -0,0 +1,192 @@
+filterSelection("all");
+
+function filterSelection(a) {
+    var d, b;
+    d = document.getElementsByClassName("filter-box");
+    if (a == "all") { a = "" }
+    for (b = 0; b < d.length; b++) { w3RemoveClass(d[b], "show"); if (d[b].className.indexOf(a) > -1) { w3AddClass(d[b], "show") } }
+}
+
+function w3AddClass(c, e) {
+    var d, a, b;
+    a = c.className.split(" ");
+    b = e.split(" ");
+    for (d = 0; d < b.length; d++) { if (a.indexOf(b[d]) == -1) { c.className += " " + b[d] } }
+}
+
+function w3RemoveClass(c, e) {
+    var d, a, b;
+    a = c.className.split(" ");
+    b = e.split(" ");
+    for (d = 0; d < b.length; d++) { while (a.indexOf(b[d]) > -1) { a.splice(a.indexOf(b[d]), 1) } }
+    c.className = a.join(" ")
+}
+var btnContainer = document.getElementById("filter");
+var btns = btnContainer.getElementsByClassName("tab-active");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function(b) {
+        var a = btnContainer.querySelectorAll(".active");
+        [].forEach.call(a, function(c) { c.classList.remove("active") });
+        b.target.classList.add("active")
+    })
+};
+
+function windowScroll() {
+    const navbar = document.getElementById("navbar");
+    if (navbar) {
+        if (
+            document.body.scrollTop >= 50 ||
+            document.documentElement.scrollTop >= 50
+        ) {
+            navbar.classList.add("nav-sticky");
+        } else {
+            navbar.classList.remove("nav-sticky");
+        }
+    }
+}
+
+window.addEventListener('scroll', (ev) => {
+    ev.preventDefault();
+    windowScroll();
+})
+
+
+// Toggle menu
+function toggleMenu() {
+    document.getElementById('isToggle').classList.toggle('open');
+    var isOpen = document.getElementById('navigation')
+    if (isOpen.style.display === "block") {
+        isOpen.style.display = "none";
+    } else {
+        isOpen.style.display = "block";
+    }
+};
+//Menu Active
+function getClosest(elem, selector) {
+    // Element.matches() polyfill
+    if (!Element.prototype.matches) {
+        Element.prototype.matches =
+            Element.prototype.matchesSelector ||
+            Element.prototype.mozMatchesSelector ||
+            Element.prototype.msMatchesSelector ||
+            Element.prototype.oMatchesSelector ||
+            Element.prototype.webkitMatchesSelector ||
+            function(s) {
+                var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                    i = matches.length;
+                while (--i >= 0 && matches.item(i) !== this) {}
+                return i > -1;
+            };
+    }
+    // Get the closest matching element
+    for (; elem && elem !== document; elem = elem.parentNode) {
+        if (elem.matches(selector)) return elem;
+    }
+    return null;
+};
+
+function activateMenu() {
+    var menuItems = document.getElementsByClassName("sub-menu-item");
+    if (menuItems) {
+        var matchingMenuItem = null;
+        for (var idx = 0; idx < menuItems.length; idx++) {
+            if (menuItems[idx].href === window.location.href) {
+                matchingMenuItem = menuItems[idx];
+            }
+        }
+        if (matchingMenuItem) {
+            matchingMenuItem.classList.add('active');
+            var immediateParent = getClosest(matchingMenuItem, 'li');
+            if (immediateParent) {
+                immediateParent.classList.add('active');
+            }
+            var parent = getClosest(matchingMenuItem, '.parent-menu-item');
+            if (parent) {
+                parent.classList.add('active');
+                var parentMenuitem = parent.querySelector('.menu-item');
+                if (parentMenuitem) {
+                    parentMenuitem.classList.add('active');
+                }
+                var parentOfParent = getClosest(parent, '.parent-parent-menu-item');
+                if (parentOfParent) {
+                    parentOfParent.classList.add('active');
+                }
+            } else {
+                var parentOfParent = getClosest(matchingMenuItem, '.parent-parent-menu-item');
+                if (parentOfParent) {
+                    parentOfParent.classList.add('active');
+                }
+            }
+        }
+    }
+}
+
+window.onload = function loader() {
+    // Menus
+    activateMenu();
+}
+
+
+
+
+
+
+
+
+
+// Contact Form
+function validateForm() {
+    var name = document.forms["myForm"]["name"].value;
+    var email = document.forms["myForm"]["email"].value;
+    var subject = document.forms["myForm"]["subject"].value;
+    var comments = document.forms["myForm"]["comments"].value;
+    document.getElementById("error-msg").style.opacity = 0;
+    document.getElementById('error-msg').innerHTML = "";
+    if (name == "" || name == null) {
+        document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Name*</div>";
+        fadeIn();
+        return false;
+    }
+    if (email == "" || email == null) {
+        document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Email*</div>";
+        fadeIn();
+        return false;
+    }
+    if (subject == "" || subject == null) {
+        document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Subject*</div>";
+        fadeIn();
+        return false;
+    }
+    if (comments == "" || comments == null) {
+        document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Comments*</div>";
+        fadeIn();
+        return false;
+    }
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("simple-msg").innerHTML = this.responseText;
+            document.forms["myForm"]["name"].value = "";
+            document.forms["myForm"]["email"].value = "";
+            document.forms["myForm"]["subject"].value = "";
+            document.forms["myForm"]["comments"].value = "";
+        }
+    };
+    xhttp.open("POST", "php/contact.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("name=" + name + "&email=" + email + "&subject=" + subject + "&comments=" + comments);
+    return false;
+}
+
+function fadeIn() {
+    var fade = document.getElementById("error-msg");
+    var opacity = 0;
+    var intervalID = setInterval(function() {
+        if (opacity < 1) {
+            opacity = opacity + 0.5
+            fade.style.opacity = opacity;
+        } else {
+            clearInterval(intervalID);
+        }
+    }, 200);
+}
